cd50c70c8d467cee192108a437df28aa79845b8b
Asad Khan <asadkhan6192@gmail.com>
2021-08-17 11:48:21 +0500

    Make Board Functional

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+function Player(name, choice) {
+    function getName() {
+        return name;
+    }
+    function getChoice() {
+        return choice;
+    }
+    function makeMove(index) {
+        Game.move(index, choice);
+    }
+    return { getName, getChoice, makeMove };
+}
+
+
 let GameBoard = (function () {
     gameBoard = [];
 
@@ -6,7 +20,10 @@ let GameBoard = (function () {
     }
 
     function render() {
-
+        boxes = document.querySelectorAll("[data-box-index]")
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].textContent = gameBoard[i];
+        }
     }
 
     function getBoard() {
@@ -18,38 +35,179 @@ let GameBoard = (function () {
         render();
     }
 
-    { init, getBoard, updateBoard };
+    return { init, getBoard, updateBoard };
 
 })();
 
+
 let Game = (function () {
-    let moves = 0;
+    let p1 = Player("Asad", "X");
+
+    const playerChoice = p1.getChoice();
+    const computerChoice = playerChoice === "X" ? "O" : "X";
 
     function checkWin() {
         let currentBoard = GameBoard.getBoard();
+        let win = 0;
+        if (currentBoard[0] === playerChoice && currentBoard[4] === playerChoice && currentBoard[8] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[0] === playerChoice && currentBoard[3] === playerChoice && currentBoard[6] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[1] === playerChoice && currentBoard[4] === playerChoice && currentBoard[7] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[2] === playerChoice && currentBoard[5] === playerChoice && currentBoard[8] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[2] === playerChoice && currentBoard[4] === playerChoice && currentBoard[6] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[3] === playerChoice && currentBoard[4] === playerChoice && currentBoard[5] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[0] === playerChoice && currentBoard[1] === playerChoice && currentBoard[2] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[6] === playerChoice && currentBoard[7] === playerChoice && currentBoard[8] === playerChoice) {
+            console.log("Player Won");
+            win = 1;
+            return true;
+        }
+
+        else if (currentBoard[0] === computerChoice && currentBoard[4] === computerChoice && currentBoard[8] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[0] === computerChoice && currentBoard[3] === computerChoice && currentBoard[6] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[1] === computerChoice && currentBoard[4] === computerChoice && currentBoard[7] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[2] === computerChoice && currentBoard[5] === computerChoice && currentBoard[8] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[2] === computerChoice && currentBoard[4] === computerChoice && currentBoard[6] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[3] === computerChoice && currentBoard[4] === computerChoice && currentBoard[5] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[0] === computerChoice && currentBoard[1] === computerChoice && currentBoard[2] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+        else if (currentBoard[6] === computerChoice && currentBoard[7] === computerChoice && currentBoard[8] === computerChoice) {
+            console.log("Computer Won");
+            win = 1;
+            return true;
+        }
+
+        if (win) removeEventListeners();
+
+        // else if(!(currentBoard.includes(undefined))){
+        //     return true;
+        // }
+
+        else {
+            return false;
+        }
     }
 
     function move(index, choice) {
         GameBoard.updateBoard(index, choice);
-        moves++;
+    }
+
+    function addEventListeners() {
+        let boxes = document.querySelectorAll(".box");
+        boxes.forEach((box) => {
+            box.addEventListener('click', playerPlay.bind(box));
+        });
+    }
+
+    function removeEventListeners() {
+        let boxes = document.querySelectorAll(".box");
+        console.log('removeEventListener called')
+        boxes.forEach((box) => {
+            box.removeEventListener('click', playerPlay);
+        });
+    }
+
+    function playerPlay() {
+        if (!checkWin()) {
+            let index = this.getAttribute('data-box-index') - 1;
+            console.log(index, playerChoice);
+            if (!this.textContent) {
+                move(index, playerChoice);
+            }
+            else {
+                return;
+            }
+            computerPlay(computerChoice);
+        }
+    }
+
+    function computerPlay(computerChoice) {
+        removeEventListeners();
+        if (!checkWin()) {
+            let index = Math.floor(Math.random() * 9);
+            let board = GameBoard.getBoard();
+            if (board[index] != undefined) {
+                while (board[index] != undefined) {
+                    index = Math.floor(Math.random() * 9);
+                }
+            }
+            console.log(index, computerChoice);
+            Game.move(index, computerChoice);
+        }
+        else {
+            return;
+        }
+    }
+
+    function play() {
+        addEventListeners();
+    }
+
+    function main() {
+        GameBoard.init();
+        Game.play();
     }
 
     function stopRendering() {
 
     }
 
+    return { checkWin, main, move, play, addEventListeners, removeEventListeners };
 
 })();
 
-function Player(name, choice) {
-    function getName() {
-        return name;
-    }
-    function getChoice() {
-        return choice;
-    }
-    function makeMove(index) {
-        Game.move(index, choice);
-    }
-    return { getName, getChoice };
-}
+Game.main();

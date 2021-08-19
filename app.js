407157471c41b68b79a12c45901a18cd1956cf39
Asad Khan <asadkhan6192@gmail.com>
2021-08-19 12:05:21 +0500

    Add display module and make reset button functional

--- app.js
+++ app.js
@@ -12,7 +12,22 @@ function Player(name, choice) {
 }
 
 
-let GameBoard = (function () {
+const displayController = (function () {
+    const winnerAnnouncer = document.querySelector("#winner-announcer");
+
+    function setDisplay(text) {
+        winnerAnnouncer.textContent = text;
+    }
+
+    function resetDisplay() {
+        winnerAnnouncer.textContent = "";
+    }
+
+    return { setDisplay, resetDisplay };
+})();
+
+
+const GameBoard = (function () {
     gameBoard = [];
 
     function init() {
@@ -34,108 +49,111 @@ let GameBoard = (function () {
         gameBoard[index] = value;
         render();
     }
-    
-    return { init, getBoard, updateBoard };
-    
+
+    function resetBtn() {
+        const btn = document.querySelector("#reset-btn");
+        btn.addEventListener('click', () => {
+            gameBoard = [];
+            render();
+            displayController.resetDisplay();
+            Game.addEventListeners();
+        })
+    }
+
+    return { init, getBoard, updateBoard, resetBtn };
+
 })();
 
 
-let Game = (function () {
-    let p1 = Player("Asad", "X");
+const Game = (function () {
+    const p1 = Player("Asad", "X");
 
     const playerChoice = p1.getChoice();
     const computerChoice = playerChoice === "X" ? "O" : "X";
-    
+
     function checkWin() {
         let currentBoard = GameBoard.getBoard();
-        
+
         if (currentBoard[0] === playerChoice && currentBoard[4] === playerChoice && currentBoard[8] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[0] === playerChoice && currentBoard[3] === playerChoice && currentBoard[6] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[1] === playerChoice && currentBoard[4] === playerChoice && currentBoard[7] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[2] === playerChoice && currentBoard[5] === playerChoice && currentBoard[8] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[2] === playerChoice && currentBoard[4] === playerChoice && currentBoard[6] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[3] === playerChoice && currentBoard[4] === playerChoice && currentBoard[5] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[0] === playerChoice && currentBoard[1] === playerChoice && currentBoard[2] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
         else if (currentBoard[6] === playerChoice && currentBoard[7] === playerChoice && currentBoard[8] === playerChoice) {
-            console.log("Player Won");
+            displayController.setDisplay("You Win!!");
             return true;
         }
 
         else if (currentBoard[0] === computerChoice && currentBoard[4] === computerChoice && currentBoard[8] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[0] === computerChoice && currentBoard[3] === computerChoice && currentBoard[6] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[1] === computerChoice && currentBoard[4] === computerChoice && currentBoard[7] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[2] === computerChoice && currentBoard[5] === computerChoice && currentBoard[8] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[2] === computerChoice && currentBoard[4] === computerChoice && currentBoard[6] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[3] === computerChoice && currentBoard[4] === computerChoice && currentBoard[5] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[0] === computerChoice && currentBoard[1] === computerChoice && currentBoard[2] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
         else if (currentBoard[6] === computerChoice && currentBoard[7] === computerChoice && currentBoard[8] === computerChoice) {
-            console.log("Computer Won");
+            displayController.setDisplay("You Loose!");
             return true;
         }
-        else if(currentBoard.length === 9 && (!currentBoard.includes(undefined))){
-            console.log(currentBoard);
-            return draw();
+        else if (currentBoard.length === 9 && (!currentBoard.includes(undefined))) {
+            displayController.setDisplay("It's a tie!")
+            return 1;
         }
         else {
             return false;
         }
     }
 
-
-    function draw(){
-        console.log("DRAW");
-        return 1;
-    }
-
     function move(index, choice) {
         GameBoard.updateBoard(index, choice);
     }
-    
+
     function addEventListeners() {
         let boxes = document.querySelectorAll(".box");
-        console.log("ADD EVENT LISTENER CALLED");
         boxes.forEach((box) => {
             box.addEventListener('click', playerPlay);
         });
@@ -143,23 +161,20 @@ let Game = (function () {
 
     function removeEventListeners() {
         let boxes = document.querySelectorAll(".box");
-        console.log('removeEventListener called');
         boxes.forEach((box) => {
             box.removeEventListener('click', playerPlay);
         });
     }
 
     function playerPlay() {
-        console.log("PLAYER PLAY CALLED");
         if (!checkWin()) {
             let index = this.getAttribute('data-box-index') - 1;
-            console.log("PLAYER MOVE : ",index, playerChoice);
             if (!this.textContent) {
                 move(index, playerChoice);
                 if (!checkWin()) {
                     computerPlay(computerChoice);
                 }
-                else{
+                else {
                     removeEventListeners();
                 }
             }
@@ -169,22 +184,15 @@ let Game = (function () {
     function computerPlay() {
         let index = Math.floor(Math.random() * 9);
         let board = GameBoard.getBoard();
-        if (board[index] != undefined) {
-            let i = 0;
-            if(i === 8){
-                draw();
-                return;
-            }
-            while (board[index] != undefined && i < board.length) {
+        if (board[index] != undefined || board[index]) {
+            while (board[index] != undefined) {
                 index = Math.floor(Math.random() * 9);
-                i++;
             }
         }
-        console.log("COMPUTER MOVE : ",index, computerChoice);
         Game.move(index, computerChoice);
-        if(checkWin()){
+        if (checkWin()) {
             removeEventListeners();
-        } 
+        }
     }
 
     function play() {
@@ -194,6 +202,7 @@ let Game = (function () {
     function main() {
         GameBoard.init();
         Game.play();
+        GameBoard.resetBtn();
     }
 
     function stopRendering() {
